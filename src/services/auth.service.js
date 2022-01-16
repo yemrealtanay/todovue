78929b1b5d4ecf8f2b1fb8c88d8c51cd1c0b7449
yemrealtanay <y.emrealtanay@gmail.com>
2022-01-16 09:21:53 +0300

    jwt auth system added and start the adjusting

---
+++ src/services/auth.service.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const API_URL = 'http://todoapi.test/api/auth/';
+
+class AuthService {
+  login(user) {
+    return axios
+      .post(API_URL + 'login', {
+        email: user.email,
+        password: user.password
+      })
+      .then(response => {
+        if (response.data.accessToken) {
+          localStorage.setItem('user', JSON.stringify((response.data)));
+        }
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+  }
+
+  register(user) {
+    return axios.post(API_URL + 'register', {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      password_confirmation: user.password_confirmation
+    });
+  }
+}
+
+  export default new AuthService();
